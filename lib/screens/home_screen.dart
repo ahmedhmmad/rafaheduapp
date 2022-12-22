ffb0107bd5b9b7607a2a3495fb45685ce46bd639
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2022-12-22 10:20:01 +0200

    signup

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final user = FirebaseAuth.instance.currentUser!;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Hello ${user.email} This is the Home Screen'),
+          MaterialButton(
+            color: Colors.blue,
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: Text(
+              "تسجيل الخروج",
+              style: GoogleFonts.cairo(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
